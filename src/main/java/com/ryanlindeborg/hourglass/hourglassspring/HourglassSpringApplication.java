ad294fc8c4a3b714658484bf465b33cc23ca5d73
ryanlindeborg <lindeborgryan@gmail.com>
2020-04-12 14:08:17 -0700

    Initialized spring project for backend of hourglass

---
+++ src/main/java/com/ryanlindeborg/hourglass/hourglassspring/HourglassSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ryanlindeborg.hourglass.hourglassspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourglassSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourglassSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanlindeborg/hourglass/hourglassspring/HourglassSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanlindeborg.hourglass.hourglassspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourglassSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
