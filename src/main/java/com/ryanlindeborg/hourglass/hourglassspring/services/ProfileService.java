1ce18a07907af4ea810e4a2643788e140f1e844f
ryanlindeborg <lindeborgryan@gmail.com>
2020-04-23 23:01:36 -0700

    Include Maven Surefire and Failsafe plugin and Mokito Junit depedency for testing

--- src/main/java/com/ryanlindeborg/hourglass/hourglassspring/services/ProfileService.java
+++ src/main/java/com/ryanlindeborg/hourglass/hourglassspring/services/ProfileService.java
@@ -7,6 +7,7 @@ import com.ryanlindeborg.hourglass.hourglassspring.model.User;
 import com.ryanlindeborg.hourglass.hourglassspring.model.api.HourglassRestErrorCode;
 import com.ryanlindeborg.hourglass.hourglassspring.model.api.ProfileJson;
 import com.ryanlindeborg.hourglass.hourglassspring.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -22,12 +23,17 @@ public class ProfileService {
     private SchoolRepository schoolRepository;
     private SchoolUserRepository schoolUserRepository;
 
+    @Autowired
     public ProfileService(UserRepository userRepository, CompanyRepository companyRepository, JobRepository jobRepository, SchoolRepository schoolRepository, SchoolUserRepository schoolUserRepository) {
         this.userRepository = userRepository;
         this.companyRepository = companyRepository;
         this.jobRepository = jobRepository;
         this.schoolRepository = schoolRepository;
         this.schoolUserRepository = schoolUserRepository;
+    }
+
+    public ProfileService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public User getUserById(Long userId) {
--- src/test/java/com/ryanlindeborg/hourglass/hourglassspring/ProfileServiceTest.java
+++ src/test/java/com/ryanlindeborg/hourglass/hourglassspring/ProfileServiceTest.java
@@ -1,4 +1,34 @@
 package com.ryanlindeborg.hourglass.hourglassspring;
 
+import com.ryanlindeborg.hourglass.hourglassspring.model.User;
+import com.ryanlindeborg.hourglass.hourglassspring.repositories.UserRepository;
+import com.ryanlindeborg.hourglass.hourglassspring.services.ProfileService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ProfileServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    ProfileService profileService;
+
+    @Test
+    public void testGetUserById() {
+        User user = new User();
+        user.setFirstName("John");
+        Optional<User> optionalUser = Optional.of(user);
+        when(userRepository.findById(Mockito.anyLong())).thenReturn(optionalUser);
+        assertEquals(user, profileService.getUserById(1L));
+    }
 }
