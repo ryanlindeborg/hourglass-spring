4a3d432b6573097d5a79c6780de530ecf380dc80
ryanlindeborg <lindeborgryan@gmail.com>
2020-05-12 21:37:27 -0700

    Add string names to Degree enum and add controller so front-end can make api call to fetch list of Degrees

---
+++ src/main/java/com/ryanlindeborg/hourglass/hourglassspring/controllers/SchoolUserRestController.java
@@ -0,0 +1,24 @@
+package com.ryanlindeborg.hourglass.hourglassspring.controllers;
+
+import com.ryanlindeborg.hourglass.hourglassspring.model.Degree;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/api/v1/schoolUser", produces="application/json")
+@CrossOrigin(origins="*")
+public class SchoolUserRestController {
+    @GetMapping("/degrees")
+    public List<String> getDegreeNames() {
+        List<String> degreeNames = new ArrayList<>();
+        for (Degree degree : Degree.values()) {
+            degreeNames.add(degree.toString());
+        }
+        return degreeNames;
+    }
+}
--- src/main/java/com/ryanlindeborg/hourglass/hourglassspring/model/Degree.java
+++ src/main/java/com/ryanlindeborg/hourglass/hourglassspring/model/Degree.java
@@ -2,6 +2,29 @@ package com.ryanlindeborg.hourglass.hourglassspring.model;
 
 public enum Degree {
 
-    MD, PhD, MBA, MS, BA, BS, JD, MFA, MPH, LLM, MPA, DO, PSYD, PHARMD
+    MD("MD"),
+    PHD("PhD"),
+    MBA("MBA"),
+    MS("MS"),
+    BA("BA"),
+    BS("BS"),
+    JD("JD"),
+    MFA("MFA"),
+    MPH("MPH"),
+    LLM("LLM"),
+    MPA("MPA"),
+    DO("DO"),
+    PSYD("PsyD"),
+    PHARMD("PharmD");
 
+    private String degreeName;
+
+    private Degree(String degreeName) {
+        this.degreeName = degreeName;
+    }
+
+    @Override
+    public String toString() {
+        return degreeName;
+    }
 }
